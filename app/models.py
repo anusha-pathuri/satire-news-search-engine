68ad3ad1134d1c50e7c05afc9d8ace84d6947806
Anusha Pathuri <170770299+anusha-pathuri@users.noreply.github.com>
2024-12-07 01:21:36 -0500

    added UI files with safe search feature

---
+++ app/models.py
@@ -0,0 +1,40 @@
+'''
+Author: Prithvijit Dasgupta
+Modified by: Anusha Pathuri
+
+This file contains the base models required by the service to function.
+'''
+from pydantic import BaseModel
+
+class QueryModel(BaseModel):
+    query:str
+
+class SearchResponse(BaseModel):
+    id: int
+    docid: int
+    score: float
+    title: str
+    text: str
+    safe: bool
+
+class PaginationModel(BaseModel):
+    prev: str
+    next: str
+
+class APIResponse(BaseModel):
+    results: list[SearchResponse]
+    page: PaginationModel | None
+
+class ExperimentResponse(BaseModel):
+    ndcg: float
+    query: str
+
+class BaseSearchEngine():
+    def __init__(self, path: str) -> None:
+        pass
+
+    def index(self):
+        pass
+
+    def search(self, query: str) -> list[SearchResponse]:
+        pass
---
+++ app/pipeline.py
@@ -0,0 +1,91 @@
+'''
+Author: Zim Gong
+Modified by: Anusha Pathuri
+'''
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from models import BaseSearchEngine, SearchResponse
+from src.document_preprocessor import RegexTokenizer
+from src.indexing import Indexer, IndexType
+from src.ranker import Ranker, BM25, TF_IDF
+from src.utils import load_txt
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
+CACHE_PATH = os.path.join(os.path.dirname(__file__), '..', '__cache__')
+
+STOPWORDS_PATH = os.path.join(DATA_DIR, 'stopwords.txt')
+DATASET_PATH = os.path.join(DATA_DIR, 'processed_articles_dedup.csv')
+
+
+def py2js_bool(value):
+    return str(value).lower()
+
+
+class SearchEngine(BaseSearchEngine):
+    def __init__(self, max_docs: int = -1, ranker: str = 'BM25') -> None:
+        print('Initializing Search Engine...')
+        self.stopwords = set(load_txt(STOPWORDS_PATH))
+
+        print('Loading indexes...')
+        self.preprocessor = RegexTokenizer('\w+')
+        self.document_index = Indexer.create_index(
+            IndexType.BasicInvertedIndex, DATASET_PATH, self.preprocessor,
+            self.stopwords, 0, max_docs=max_docs, text_key='body'
+        )
+        self.title_index = Indexer.create_index(
+            IndexType.BasicInvertedIndex, DATASET_PATH, self.preprocessor,
+            self.stopwords, 0, max_docs=max_docs, text_key='headline'
+        )
+
+        print('Loading ranker...')
+        self.set_ranker(ranker)
+
+        print('Search Engine initialized!')
+
+    def set_ranker(self, ranker: str = 'BM25') -> None:
+        if ranker == 'BM25':
+            self.scorer = BM25(self.document_index)
+        elif ranker == 'TF_IDF':
+            self.scorer = TF_IDF(self.document_index)
+        else:
+            raise ValueError("Invalid ranker type")
+        
+        self.ranker = Ranker(self.document_index, self.preprocessor, self.stopwords, 
+                             self.scorer, score_top_k=100)
+        self.pipeline = self.ranker
+
+    def search(self, query: str) -> list[SearchResponse]:
+        # 1. Use the ranker object to query the search pipeline
+        # 2. This is example code and may not be correct.
+        results = self.pipeline.query(query)
+        return [
+            SearchResponse(id=idx+1, 
+                           docid=result[0], 
+                           score=result[1], 
+                           title=self.title_index.get_doc_text(result[0]), 
+                           text=self.document_index.get_doc_text(result[0]),
+                           safe=np.random.choice([py2js_bool(True), py2js_bool(False)]))   # TODO: Get safe status from the dataset
+            for idx, result in enumerate(results)
+        ]
+
+
+def initialize():
+    search_obj = SearchEngine(max_docs=1000)
+    return search_obj
+
+
+def main():
+    search_obj = SearchEngine(max_docs=1000)
+    search_obj.set_ranker('BM25')
+    query = "american"
+    results = search_obj.search(query)
+    print(len(results))
+    for result in results[:5]:
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
